52e7129ee4def4a79eea4956ec281839e2c01f87
syedmeesamali <syedmeesamali@yahoo.com>
2019-10-04 22:51:31 +0400

    Use of pointers - memory referencing

--- 1_Initial/first/main.c
+++ 1_Initial/first/main.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
-void go_south_east(int lat, int lon)
+void go_south_east(int* lat, int* lon)
 {
-    lat = lat - 1;
-    lon = lon + 1;
+    *lat = *lat - 1;
+    *lon = *lon + 1;
 }
 
 int main()
@@ -10,7 +10,7 @@ int main()
     int latitude = 30;
     int longitude = 45;
 
-    go_south_east(latitude, longitude);
+    go_south_east(&latitude, &longitude);
     printf("Now lat is: %i and long is %i\n", latitude, longitude);
     printf("No change from declared values .... ))))");
     return 0;
