59350ec3bbb9f25f573515d4a2e823c99ef2709a
syedmeesamali <syedmeesamali@yahoo.com>
2019-10-08 23:35:01 +0400

    scanf for pointers

--- 1_Initial/pointers/main.c
+++ 1_Initial/pointers/main.c
@@ -1,28 +1,16 @@
 #include <stdio.h>
 
-void skip(char *msg)
-{
-    puts((msg+6));
-}
-
 int main()
 {
-
-    /*
-    int contest[] = {1,2,3};
-    int *choice = contest;
-    contest[0] = 2;
-    contest[1] = contest[2];
-    contest[2] = *choice;
-    printf("Pick contestant number %i\n", contest[2]);
-    */
 
     /*
     int doses[] = {1, 3, 2, 1000};
     printf("Issue doses %i", 3[doses]);
     return 0;
     */
-    char *msg_from = "This is the correct message";
-    skip(msg_from);
-
+    char name[40];
+    printf("Enter your name: ");
+    scanf("%s", name);
+    printf("Your name is %s", name);
+    return 0;
 }
