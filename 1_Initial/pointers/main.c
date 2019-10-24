001e1ecbe172fb7d74f1975a2eddfed04347aa1d
syedmeesamali <syedmeesamali@yahoo.com>
2019-10-24 13:02:41 +0400

    small but important change and all is fine

--- 1_Initial/pointers/main.c
+++ 1_Initial/pointers/main.c
@@ -5,12 +5,11 @@ int main()
     char masked[] = "Alive";
     char *jimmy = masked;
     printf("Masked is: %s and raider is %s\n", masked, jimmy);
-    masked[0] = "d";
-    masked[1] = "e";
-    masked[2] = "a";
-    masked[3] = "d";
-    masked[4] = "!";
+    masked[0] = 'd';
+    masked[1] = 'e';
+    masked[2] = 'a';
+    masked[3] = 'd';
+    masked[4] = '!';
     printf("Masked is: %s and raider is %s", masked, jimmy);
     return 0;
 }
-
