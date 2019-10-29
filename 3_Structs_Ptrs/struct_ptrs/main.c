14899186c114c2e0ca03ec9fa45f8b221bc02d97
syedmeesamali <syedmeesamali@yahoo.com>
2019-10-29 23:51:10 +0400

    linked list with a push

--- 3_Structs_Ptrs/struct_ptrs/main.c
+++ 3_Structs_Ptrs/struct_ptrs/main.c
@@ -1,21 +1,29 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+//Define the structure node
 struct Node
 {
     int data;
     struct Node* next;
 };
-
+//Print the list
 void printList(struct Node* n)
 {
     while (n != NULL)
     {
-        printf(" %d ", n -> data);
+        printf("\n %d ", n -> data);
         n = n -> next;
     }
 }
-
+//Push new node to the linked list
+void push(struct Node** head_ref, int new_data)
+{
+    struct Node* new_node = (struct Node*)malloc(sizeof(struct Node));
+    new_node -> data = new_data;
+    new_node -> next = (*head_ref);
+    (*head_ref) = new_node;
+}
+//Main program for node initialization in linked list
 int main()
 {
     struct Node* head = NULL;
@@ -32,5 +40,8 @@ int main()
     second -> next = third;
     third -> data = 17;
     third -> next = NULL;
+    printList(head);
+    printf("\nAfter addition of new node!\n");
+    push(&head, 24);
     printList(head);
 }
