aab575b699ec7e253ba749afb7f9adb23888d48c
syedmeesamali <syedmeesamali@yahoo.com>
2019-11-04 01:52:28 +0400

    more play with linkedlists in C

--- 2_LinkedList/list2/main.c
+++ 2_LinkedList/list2/main.c
@@ -8,6 +8,7 @@ typedef struct node
 } node;
 
 node * createList(int n);
+void dispList(node * head);
 
 int main()
 {
@@ -15,20 +16,45 @@ int main()
     node * head = NULL;
     printf("\nHow many nodes? ");
     scanf("%d", &n);
+    head = createList(n);
+    dispList(head);
     return 0;
 }
 
 node * createList(int n)
 {
     int i = 0;
-    node * head = NULL;
-    node * temp = NULL;
-    node * p = NULL;
+    node* head = NULL;
+    node* temp = NULL;
+    node* p = NULL;
     for (i=0; i<n; i++)
     {
         temp = (node*)malloc(sizeof(node));
-        printf("\nEnter data for node: ");
-        scanf("%d", &(temp->data));
+        printf("\nEnter data for node %d: ", i);
+        scanf("%d", &(temp->value));
         temp->next = NULL;
+
+        if (head == NULL)
+        {
+            head = temp;
+        } else {
+            p = head;
+            while (p->next != NULL)
+            {
+                p = p->next;
+                p->next = temp;
+            }
+        return head;
+        }
+    }
+}
+
+void dispList(node * head)
+{
+    node * p = head;
+    while (p != NULL)
+    {
+        printf(" %d -", p->value);
+        p = p->next;
     }
 }
