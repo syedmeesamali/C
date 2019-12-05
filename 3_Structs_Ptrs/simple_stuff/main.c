c11480437a70d698d00519229a7cf8a6832cb58b
syedmeesamali <syedmeesamali@yahoo.com>
2019-12-05 13:33:18 +0400

    Some play with C and realloc of arrays

--- 3_Structs_Ptrs/simple_stuff/main.c
+++ 3_Structs_Ptrs/simple_stuff/main.c
@@ -1,14 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+/*
 int power(int m, int n);
-
 int main()
 {
     int i;
     for (i=0; i<10; ++i)
     {
-        printf("%d %d %d\n", i, power(2,i), power(-3,i));
+        printf("%d %5d %6d\n", i, power(2,i), power(-3,i));
     }
     return 0;
 }
@@ -22,4 +21,23 @@ int power(int base, int n)
         p = p * base;
     }
     return p;
+}*/
+
+int strlen(char *s)
+{
+    int n;
+    for (n=0; *s != '\0'; s++)
+    {
+        n++;
+    }
+    return n;
+}
+
+int main()
+{
+    char a[10];
+    printf("Enter a word: ");
+    fgets(a, sizeof(a), stdin);
+    printf("\nWord %s has %d chars:", a, strlen(a));
+    return 0;
 }
