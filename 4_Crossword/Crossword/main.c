4fe39011ea81455c1624ce301263e818774f4fa8
syedmeesamali <syedmeesamali@yahoo.com>
2019-12-29 02:25:11 +0400

    Crossword puzzle done

--- 4_Crossword/Crossword/main.c
+++ 4_Crossword/Crossword/main.c
@@ -1,44 +1,37 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-
 int main()
-{
-    int m = 0;
-    int n = 0;
-    char matrix[11][11] ={"A L O V E M",
-                          "K G I V E P",
-                          "A F S W G Q",
-                          "N R I U C S",
-                          "O L K O D T",
-                          "Z L I L E M",
-                          };
-    //Below is a list to be searched within main cross-board letters
-    char search[9][4] ={"L O V E",
-                      "I S I",
-                      "G I V E",
-                      "N O",
-                      };
-    //First loop to print the original list
-    for (m=0; m<11; m++)
-    {
-        for (n=0; n<11; n++)
-        {
-            printf("%c", matrix[m][n]);
-        }
-        printf("\n");
+{   int m = 0; int n = 0;
+    int i = 0; int j = 0;
+    char transpose[6][6];
+    char matrix[6][6] ={  "ALOVEM",
+                          "KGIVEP",
+                          "AFSWGQ",
+                          "NRIUCS",
+                          "OLKODT",
+                          "ZLILEM", };  //Below is a list to be searched within main cross-board letters
+    char search[4][9] ={"LOVE","ISI","GIVE","NO",};
+    printf("Original matrix print: \n");
+    for (m=0; m<6; m++)
+    {  for (n=0; n<6; n++)
+        { printf("%c", matrix[m][n]);
+        } printf("\n");  }
+    printf("Words found in matrix: \n");
+    for (i=0; i<4; i++)
+    {  for (j=0; j<6; j++)
+        {   if (strstr(matrix[j], search[i]))
+            {   printf("Horizontally Found: %s \n", search[i]);
+                break;   } }
     }
-    //This is the search list
-    for (m=0; m<11; m++)
-    {
-        for (n=0; n<11; n++)
-        {
-            if (strstr(matrix[m][n], search[m][n]))
-            {
-                printf("Found match at %d, %d", m, n);
-            }
-        }
-        printf("\n");
+    for (i=0; i<6; i++)
+    {   for (j=0; j<6; j++)
+        {  transpose[i][j] = matrix[j][i];   }
     }
-    return 0;
-}
+    for (i=0; i<4; i++)
+    {   for (j=0; j<6; j++)
+        {   if (strstr(transpose[j], search[i]))
+            {   printf("Vertically Found: %s \n", search[i]);
+                break;   } }
+    } return 0;  }
+
