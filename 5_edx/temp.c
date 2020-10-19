8b3920a3514c8d9670c6be4f9966416a3548588a
syedmeesamali <syedmeesamali@yahoo.com>
2020-10-20 01:26:11 +0400

    Re-learning or revision of C concepts is fun - not a forgiving language

--- 5_edx/temp.c
+++ 5_edx/temp.c
@@ -1,34 +1,32 @@
 #include<stdio.h>
-int main(void)
-{
-	int userTemp;
-	char unit;
-	double convertedTemp;
-	scanf("%d %c", &userTemp, &unit);
-	if (unit == 'C')
-	{
-		//Function to calculate the fahrenheit
-		convertedTemp = double(degToF(userTemp));
-//		convertedTemp = 35.5;
-		printf("Temp in F is %0.2\n", convertedTemp);
-	} else if (unit == 'F')
-	{
-		//Function for Centigrade calculation
-		convertedTemp = double(FtoDeg(userTemp));
-//		convertedTemp = 35.5;
-		printf("Temp in C is %0.2\n", convertedTemp);
-	}
-	return 0;
-} //End of main
-
 //Degree to fahrenheit
 double degToF(int x)
 {
-	return((9.0/5)*x + 32);
+	return ((9.0/5)*x + 32);
 }
 
 //Fahrenheit to degrees
 double FtoDeg(int x)
 {
-	return(5.0/9*(x-32));
+	return (5.0/9*(x-32));
 }
+
+int main(void)
+{
+	int userTemp = 0;
+	char unit;
+	double convertedTemp = 1.0;
+	scanf("%d %c", &userTemp, &unit);
+	if (unit == 'C')
+	{
+		//Function to calculate the fahrenheit
+		printf("Temp in C is %0.2f\n", degToF(userTemp));
+	} else if (unit == 'F')
+	{
+		//Function for Centigrade calculation
+		printf("Temp in F is %0.2f\n", FtoDeg(userTemp));
+	}
+	return 0;
+} //End of main
+
+
