eadf97e16bacb28b5fd8f18f98536dac8e5da621
syedmeesamali <syedmeesamali@yahoo.com>
2020-10-21 06:43:28 +0400

    Great fun with the math library as well as some more learning

--- 5_edx/ref1.c
+++ 5_edx/ref1.c
@@ -1,4 +1,5 @@
 #include<stdio.h>
+#include<math.h>
 
 int main(void)
 {
@@ -7,10 +8,13 @@ int main(void)
 	printf("Enter some message");
 	ch = getchar();
 	while (ch != '\n')
-    {
-        len++;
-        ch = getchar();
-    }
-    printf("Your message was %d long", len);
+	{
+		len++;
+		ch = getchar();
+	}
+	printf("Your message was %d long \n", len);
+	double a = 2.5;
+	double a2 = pow(a, 2);
+	printf("%.2lf squared is %.2lf \n", a, a2);
 	return 0;
 } //End of main
