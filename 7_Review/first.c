bae19d62e51b975b19173b67b4bc30dafd05b22b
syedmeesamali <syedmeesamali@yahoo.com>
2021-03-06 23:50:04 +0400

    Review of C

---
+++ 7_Review/first.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+function add(int a, int b)
+{
+    return a + b;
+}
+
+
+int main()
+{
+    int a; int b; int sum;
+    printf("Review of C again!");
+    printf("Enter first number!");
+    scanf("%d", a);
+    printf("Enter second number!");
+    scanf("%d", b);
+    sum = add(a,b);
+    printf("Sum is %d", sum);
+    return 0;
+}
+
