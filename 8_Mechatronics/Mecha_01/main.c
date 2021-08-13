74b7038884aea43fb7f563d1daf16f2564e57701
syedmeesamali <syedmeesamali@yahoo.com>
2021-08-13 19:47:34 +0400

    arrays and array modification by passing using pointers

--- 8_Mechatronics/Mecha_01/main.c
+++ 8_Mechatronics/Mecha_01/main.c
@@ -1,13 +1,25 @@
 #include <stdio.h>
 
+//Function to modify array
+void modify_array(float *local_array)
+{
+    local_array[2] = 3.5;
+    return;
+}
+
+
+//Main function
 int main()
 {
-    int miles, yards;
-    float km;
-    miles = 26;
-    yards = 285;
-    km = 1.069 * (miles + (yards / 1760.0));
-
-    printf("A marathon is %f KM\n", km);
+    float orig_array[] = {5, 6, 7, 8};
+    for (int i=0; i<4; i++)
+    {
+        printf("Element %d of original array is %f \n", i, orig_array[i]);
+    }
+    modify_array(orig_array);
+    for (int i=0; i<4; i++)
+    {
+        printf("Element %d of changed array is %f \n", i, orig_array[i]);
+    }
     return 0;
 }
