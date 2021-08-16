f2a6eb239dd9e78971424ade06981c373ede37d7
syedmeesamali <syedmeesamali@yahoo.com>
2021-08-16 22:04:03 +0400

    Simple items in C programs

---
+++ 8_Mechatronics/Mecha_02/main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+    char str[4] = "ali";
+    for (int i=0; i<4; i++)
+    {
+        printf("%c", str[i]);
+    }
+    printf("\nPrinting of chars is done!");
+    return 0;
+}
