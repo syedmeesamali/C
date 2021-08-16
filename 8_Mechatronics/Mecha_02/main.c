551f171031e2266667f094ed75d1ae2192955a31
syedmeesamali <syedmeesamali@yahoo.com>
2021-08-16 23:55:10 +0400

    Initial look at strings and pointers - C

--- 8_Mechatronics/Mecha_02/main.c
+++ 8_Mechatronics/Mecha_02/main.c
@@ -3,10 +3,18 @@
 int main()
 {
     char str[4] = "ali";
+    int a = 25;
+    int *p = &a;        //Pointer
+    printf("\n - a is %d", a);
+    printf("\n - pointer to a is %d", p);
+    printf("\n - address of a is %d\n", &a);
+
+    char myString[] = "\n - This is full \t string including tab. \n";
     for (int i=0; i<4; i++)
     {
         printf("%c", str[i]);
     }
     printf("\nPrinting of chars is done!");
+    printf("\nSTRART-- \n %s ---END \n", myString);
     return 0;
 }
