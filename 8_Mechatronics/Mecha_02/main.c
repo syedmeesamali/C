52344f89e7ec206699cf1db8a991099e1067d7f1
syedmeesamali <syedmeesamali@yahoo.com>
2021-08-17 23:39:02 +0400

    Bitwise and & or

--- 8_Mechatronics/Mecha_02/main.c
+++ 8_Mechatronics/Mecha_02/main.c
@@ -1,13 +1,18 @@
 #include <stdio.h>
+#define val1 0x01
+#define val2 0x02
 
 int main()
 {
     char str[4] = "ali";
     int a = 25;
+    int val3;
     int *p = &a;        //Pointer
+    val3 = val1 | val2;
     printf("\n - a is %d", a);
     printf("\n - pointer to a is %d", p);
     printf("\n - address of a is %d\n", &a);
+    printf("\n - Val3 is %d \n", val3);
 
     char myString[] = "\n - This is full \t string including tab. \n";
     for (int i=0; i<4; i++)
