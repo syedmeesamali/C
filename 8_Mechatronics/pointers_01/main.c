038ab8a63075574c8f9a943a16bb091c8d7154fa
syedmeesamali <syedmeesamali@yahoo.com>
2021-08-21 17:55:06 +0400

    Pointer arithmetic

--- 8_Mechatronics/pointers_01/main.c
+++ 8_Mechatronics/pointers_01/main.c
@@ -5,8 +5,17 @@ int main()
 {
     int a;
     int *p;
-    p = &a;         //&a is address of variable a (int)
-    printf("%d \n", p);
-    printf("%d is Value of a\n", *p);       //Value at address pointed at by p
+    a = 10;
+    p = &a; //&a is the address of a
+    printf("Address of p: %d \n", p);
+    printf("Value at p is: %d \n", *p);       //Value at address pointed at by p
+    int b = 20;
+    *p = b;
+    printf("Address of p: %d \n", p);
+    printf("Address of p+1 is: %d \n", p+1);
+    printf("Value at p is: %d \n", *p);       //Value at address pointed at by p
+    printf("Size of int is: %d \n", sizeof(int));
+    //So the value of ADDRESS is NOT changed but the value of POINTED TO is changed
+
     return 0;
 }
