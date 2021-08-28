c3da759e612f465b00622287700cdf2cdff86e96
syedmeesamali <syedmeesamali@yahoo.com>
2021-08-28 23:48:56 +0400

    Review of string library

--- 8_Mechatronics/Array_pointer.c
+++ 8_Mechatronics/Array_pointer.c
@@ -2,10 +2,9 @@
 int SumOfElements(int *a, int size)
 {
     int i, sum = 0;
-    printf("\nSize of a: %d, Size of a[0] = %d", sizeof(a), sizeof(a[0]));
     for (i=0; i<size; i++)
     {
-        sum += a[i];
+        a[i] = 2 * a[i];
     }
     return sum;
 }
@@ -16,6 +15,10 @@ int main()
     int size = sizeof(a) / sizeof(a[0]);
     int total = SumOfElements(a, size);
     printf("\nSum of elements in array: %d", total);
-    printf("\nSize of A: %d, Size of a[0] = %d", sizeof(a), sizeof(a[0]));
+    printf("\nSize of A-: %d, Size of a[0] = %d", sizeof(a), sizeof(a[0]));
+    for (int i=0; i<size; i++)
+    {
+        printf("\n %d ", a[i]);
+    }
     return 0;
 }
---
+++ 8_Mechatronics/sting_arr.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+    char c[] = "meesam";
+    int len = strlen(c);
+    printf("\n String %s has length %d", c, len);
+    return 0;
+}
