c3cb7811ad53cd9e685039398b7e7e66bf523182
syedmeesamali <syedmeesamali@yahoo.com>
2021-10-13 23:55:39 +0400

    Some further struggle with C and memory in C

--- 7_Memory/Memory01/main.c
+++ 7_Memory/Memory01/main.c
@@ -6,6 +6,11 @@ int main(void)
 {
     int n = 50;
     int *p = &n;
+    char *s = "meesam";
     printf("%i -- %p\n", n, p);
+    printf("%s\n", s);
+    printf("%c\n", s[3]);
+    printf("%c\n", *s);
+    printf("%c\n", *(s+1));
     return 0;
 }
