79c25f708b406d9c4ea388ddd69c8b7076bb5289
syedmeesamali <syedmeesamali@yahoo.com>
2023-06-23 20:27:55 +0400

    updated study of C agin from coursera

---
+++ Misc/pi.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#define PI 3.142
+/* program body*/
+int main(void)
+{
+	int radius = 5;
+	float area;
+	printf("Radius of cricle = %d\n", radius);
+    area = PI * radius * radius;
+    printf("Area will be %f\n", area);
+    return 0;
+}
+	
