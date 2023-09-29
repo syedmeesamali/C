55ac3a17f222300330716bf070a6bd6f73a60706
syedmeesamali <syedmeesamali@yahoo.com>
2023-09-29 23:44:29 +0300

    Classic review continues

---
+++ 0_Coursera/checks_foundation_2.c
@@ -0,0 +1,11 @@
+/* C Programming
+Foundation checks (First course of 7 part series)
+*/
+
+#include <stdio.h>
+
+int main(void)
+{
+    printf("3*2 equals %d and 3-2 equals %d", 3*2, 3-2);
+    return(0);
+}
