7904dcb38cc56fc6add14760ced069f05a1a48c6
syedmeesamali <syedmeesamali@yahoo.com>
2023-10-02 23:14:11 +0300

    Checks continues

--- 0_Coursera/checks_foundation_3.c
+++ 0_Coursera/checks_foundation_3.c
@@ -5,6 +5,13 @@ Foundation checks (First course of 7 part series)
 #include <stdio.h>
 int main(void)
 {
-    printf("Dear Procrastinator,\nYou still have to wait for %d days (%d minutes or %d seconds) before you can procrastinate!", 25-23, (25-23)*60*24,(25-23)*60*60*24);
+    int balance;
+    balance = 50;       //starting value
+    printf("I have %d dollars in my account!\n", balance);
+    //Expense of 40
+    balance = balance - 40;
+    printf("I have %d dollars in my account!\n", balance);
+    balance = balance + 20;
+    printf("I have %d dollars in my account!\n", balance);
     return(0);
 }
