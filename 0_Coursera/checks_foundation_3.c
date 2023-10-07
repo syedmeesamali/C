0fcad31244a49103a322eee911c370b725079166
syedmeesamali <syedmeesamali@yahoo.com>
2023-10-07 23:10:03 +0300

    Review continues - C works

--- 0_Coursera/checks_foundation_3.c
+++ 0_Coursera/checks_foundation_3.c
@@ -1,5 +1,15 @@
 /* C Programming
 Foundation checks (First course of 7 part series)
+Variable names can use:
+    lowercase and uppercase letters (characters) and digits
+    do not use special characters like @ # & " ...
+    do not use accented characters like é è à ...
+    do not start with a digit
+    start only with a letter
+    spaces are forbidden
+     _ may be used instead of a space in the name of the variable
+    YouCanUseUppercaseLettersBetweenWordsInsteadOfSpaces
+
 */
 
 #include <stdio.h>
