fbc3434940c343a1714e0cb12e40fa2edd3d431f
syedmeesamali <syedmeesamali@yahoo.com>
2023-10-08 23:19:20 +0300

    C lets u See

--- 0_Coursera/checks_foundation_3.c
+++ 0_Coursera/checks_foundation_3.c
@@ -15,13 +15,22 @@ Variable names can use:
 #include <stdio.h>
 int main(void)
 {
-    int balance;
-    balance = 50;       //starting value
-    printf("I have %d dollars in my account!\n", balance);
-    //Expense of 40
-    balance = balance - 40;
-    printf("I have %d dollars in my account!\n", balance);
-    balance = balance + 20;
-    printf("I have %d dollars in my account!\n", balance);
-    return(0);
+    /*
+    int i;
+    int numberOfHazelnuts = 0;
+    int distanceTraveled = 0;
+    for(i = 0; i < 9 ; i++)
+    {
+        printf("At %d miles I have %d hazelnuts.\n", distanceTraveled, numberOfHazelnuts);
+        distanceTraveled = distanceTraveled + 1;
+        numberOfHazelnuts = numberOfHazelnuts + 3;
+    }
+    */
+    int i = 0;
+    //Print the table for 8 multiplication
+    for (i = 0; i <= 10; i++)
+    {
+        printf("%dx8 = %d\n", i, i*8);
+    }
+    return 0;
 }
