ec5a71959df855b7bf0a0608d12d33ca134f391f
syedmeesamali <syedmeesamali@yahoo.com>
2023-11-24 20:05:27 +0300

    assignment done

--- 0_Coursera/2_Memory/checks_ref.c
+++ 0_Coursera/2_Memory/checks_ref.c
@@ -3,19 +3,19 @@ Checks only
 */
 
 #include <stdio.h>
-int findSize(int a, char c);
+int add(int a, int b);
 int main(void)
 {
-    int val = 0;
-    char type;
-    int kb = 0;
-    int mb = 0;
-    int sum = 0;
-    scanf("%c %d", &type, &val);
-    sum = sizeof(double) * val;
-    mb = sum / 1000000;
-    kb = (sum - mb * 1000000) / 1000;
-    printf("\nBytes are %d", sum);
-    printf("\n%d MB %d KB", mb, kb);
+    int a = 5;
+    int b = 7;
+    a = add(a, b);
+    printf("%d", a);
     return 0;
 } //End of main
+
+//Addition function
+int add(int a, int b)
+{
+    int c = 2;
+    return(c*a+b);
+}
