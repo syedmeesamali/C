519bc44d9e477d20e02453b5e8d3c1a79ba10f9b
syedmeesamali <syedmeesamali@yahoo.com>
2023-12-27 13:03:27 +0500

    DEF usage

--- 0_Coursera/2_Memory/C_Reviews_01.c
+++ 0_Coursera/2_Memory/C_Reviews_01.c
@@ -3,22 +3,16 @@ Reviews of the language
 */
 
 #include <stdio.h>
+#define LOWER 0
+#define UPPER 300
+#define STEP 20
+
 int main(void)
 {
-    float fahr, cels;
-    int lower, upper, step;
-    //Set lower and upper limits
-    lower = 0;
-    upper = 300;
-    step = 20;
-
-    //Conversion table
-    fahr = lower;
-    while (fahr < upper)
+    int fahr;
+    for (fahr = LOWER; fahr <= UPPER; fahr += STEP)
     {
-        cels = (5.0 / 9.0) * (fahr - 32.0);
-        printf("%3.0f \t%6.1f\n", fahr, cels);
-        fahr += step;
+        printf("%3d \t%6.1f\n", fahr, (5.0 / 9.0) * (fahr - 32.0));
     }
 	return 0;
 }
