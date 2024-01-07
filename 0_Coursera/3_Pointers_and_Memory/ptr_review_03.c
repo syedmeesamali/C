3e093fd15d4949881520a54c647a462abbc6e92e
syedmeesamali <syedmeesamali@yahoo.com>
2024-01-07 23:54:50 +0300

    Pointers review continues

--- 0_Coursera/3_Pointers_and_Memory/ptr_review_03.c
+++ 0_Coursera/3_Pointers_and_Memory/ptr_review_03.c
@@ -10,8 +10,19 @@ int main(void)
     int * iAddr = &i;
     int * jAddr = &j;
     int * kAddr = &k;
+    double b = 3.141;
+    double * dAddr = &b;
+    char c = 'M';
+    char * cAddr = &c;
     printf("%d variable stored at address %p & j at %p \n", i, iAddr, jAddr);
     printf("%d variable stored at address at %p \n", k, kAddr);
-
+    printf("%lf double stored at address at %p \n", b, dAddr);
+    printf("%c character stored at address at %p \n", c, cAddr);
+    myFunction(iAddr, dAddr, cAddr);
 	return 0;
+}
+
+void myFunction(int *iptr, double *aptr, char * cptr)
+{
+    printf("Function received address: %p, %p and %p", iptr, aptr, cptr);
 }
