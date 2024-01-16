f43992577cf0312c8dd32e64436e3a5bbe9ffe84
syedmeesamali <syedmeesamali@yahoo.com>
2024-01-16 22:50:35 +0300

    Further review of pointers - Quiz DONE

--- 0_Coursera/3_Pointers_and_Memory/ptr_review_03_deref.c
+++ 0_Coursera/3_Pointers_and_Memory/ptr_review_03_deref.c
@@ -6,30 +6,20 @@ int main(void)
 {
 	int i = 42;
     int j = 52;
-
     int * iAddr = &i;
     int * jAddr = &j;
-
-
     printf("%d variable stored at address %p! \n", *iAddr, iAddr);
     printf("%d variable stored at address at %p \n", *jAddr, jAddr);
-
-    myFunction(iAddr, jAddr);
     doubleThem(iAddr, jAddr);
 	return 0;
-}
-
-void myFunction(int *iptr, int *aptr)
-{
-    printf("Function received address: %p, %p - Values %d and %d\n", iptr, aptr, *iptr, *aptr);
 }
 
 void doubleThem(int *iptr, int *aptr)
 {
     int *bptr = iptr;
     int *cptr = aptr;
-    *bptr = *bptr * 2;
-    *cptr = *cptr * 2;
-    printf("Values after DOUBLE function = %d and %d", *iptr, *aptr);
+    *bptr = *bptr * (*bptr);
+    *cptr = *cptr * (*cptr);
+    printf("Values after SQUARE function = %d and %d", *iptr, *aptr);
 }
 
