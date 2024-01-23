82920f3545b4e39f6b432572e44c3dbe9e87755e
syed <syed@Ubuntu.myguest.virtualbox.org>
2024-01-23 23:41:10 +0300

    from ubuntu home noW

---
+++ 0_Coursera/3_Pointers_and_Memory/triangle.c
@@ -0,0 +1,12 @@
+#include<gl.h>
+void renderit(void)
+{
+	glClear(GL_COLOR_BUFFER_BIT);
+	glBegin(GL_TRIANGLES);
+	glColor3f(0.0f, 0.0f, 1.0f);
+	glVertex3f(-0.5, 0.0, 0.0);
+	glVertex3f(0.5, 0.0, 0.0);
+	glVertex3f(0.0, 0.5, 0.0);
+	glEnd();
+	glFlush();
+}
