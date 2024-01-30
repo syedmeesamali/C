32174e702517e4a46cb915657094ac761b571402
syedmeesamali <syedmeesamali@yahoo.com>
2024-01-30 15:56:25 +0300

    pointers with arrays - ubuntu gcc

--- 0_Coursera/3_Pointers_and_Memory/ptr_array.c
+++ 0_Coursera/3_Pointers_and_Memory/ptr_array.c
@@ -1,10 +1,25 @@
 #include<stdio.h>
+void reverseArray(int *, int);
 int main()
 {
     int arr[] = {3, 5, 7, 9, 11, 13};
-    int *ptr = arr;
-    printf("First element of array is %d\n", arr[0]);
-     *ptr = 0;
-    printf("Now after change First element of array is %d\n", arr[0]);
-   return 0;
+    int n = 6;
+    for (int i=0; i<5; i++)
+    {
+        printf("arr[%d] = %d || ", i, arr[i]);
+    }
+    printf("arr[%d] = %d", 5, arr[5]);
+    reverseArray(arr, 6);
+    return 0;
+}
+
+//Reverse the array function
+void reverseArray(int *ptr, int n)
+{
+    printf("\nReversed array!\n");
+    for (int i=5; i>0; i--)
+    {
+        printf("arr[%d] = %d || ", i, *(ptr+i));
+    }
+    printf("arr[%d] = %d", 0, *(ptr+0));
 }
