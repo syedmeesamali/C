32d7a30ee0389c81620bff5eb59b81f0b8352a68
syed <syedmeesamali@yahoo.com>
2024-02-24 23:46:38 +0300

    Words in reverse with 40 char limit each - Ubuntu home

--- 0_Coursera/3_Pointers_and_Memory/assign_reverse.c
+++ 0_Coursera/3_Pointers_and_Memory/assign_reverse.c
@@ -1,14 +1,14 @@
 #include<stdio.h>
 int main()
 {
-    char words[5][10];
+    char words[68][40];
     int i, j;
-    for (i=0; i<5; i++)
+    for (i=0; i<68; i++)
     {
-        scanf("%s", &words[i][10]);
+        scanf("%s", &words[i][40]);
     }
-    printf("\nWords as reversed are: \n");
-    for (i=5; i>=0; i--)
+    //Print the reverse below
+    for (i=67; i>=0; i--)
     {
         printf("%s ", words[i]);
     }
