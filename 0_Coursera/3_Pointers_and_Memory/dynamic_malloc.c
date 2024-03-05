1178db548514a35a9fec20e3d0db236f449d9797
syed <syedmeesamali@yahoo.com>
2024-03-05 23:33:08 +0300

    Possible assignment solution for dynamic memory allocation - Jammy (home)

--- 0_Coursera/3_Pointers_and_Memory/dynamic_malloc.c
+++ 0_Coursera/3_Pointers_and_Memory/dynamic_malloc.c
@@ -1,11 +1,19 @@
 #include<stdio.h>
+#include<stdlib.h>
 char * allocateString(int);
 int main()
 {
     int lenLight, lenDark;
     char *strLight, *strDark;
     scanf("%d %d", &lenLight, &lenDark);
-    allocateString(lenLight);
+    strLight = allocateString(lenLight);    //allocate memory to store the light variable
+    strDark = allocateString(lenDark);      //allocate memory for the dark label variable
+    scanf("%s", strLight);
+    scanf("%s", strDark);
+    printf("Local settings: %s - %s\n", strLight, strDark);
+    free(strLight);
+    free(strDark);
+    
     return 0;
 }
 
@@ -13,6 +21,6 @@ int main()
 char *allocateString(int numChars)
 {
     char * ptr;
-    printf("\nLight chars are %d\n", numChars);
+    ptr = (char *) malloc(numChars * sizeof(char));
     return ptr;
 }
