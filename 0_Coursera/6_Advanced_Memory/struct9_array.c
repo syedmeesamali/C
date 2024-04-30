4a85840a37b7f2b9c63a27fc3c9f8bcba4fdfe17
syedmeesamali <syedmeesamali@yahoo.com>
2024-04-30 23:39:19 +0300

    Some further struct review with structs having arrays

---
+++ 0_Coursera/6_Advanced_Memory/struct9_array.c
@@ -0,0 +1,28 @@
+/* C Advanced Programming
+Structure of arrays.
+*/
+#include <stdio.h>
+struct point
+{
+    int x;
+    int y;
+};
+
+
+//Function prototypes
+void printPoint(struct point pt);
+void readPoint(struct point *pt);
+
+//Triangle struct
+struct Triangle
+{
+
+};
+
+
+//Main function
+int main(void)
+{
+	struct point z;
+	return 0;
+}
