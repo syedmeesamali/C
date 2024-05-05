52f035499ad5eff141ecd1a3ed943efa6f78eec5
syedmeesamali <syedmeesamali@yahoo.com>
2024-05-05 23:50:57 +0300

    More with pointers and functions

--- 0_Coursera/6_Advanced_Memory/struct9_array.c
+++ 0_Coursera/6_Advanced_Memory/struct9_array.c
@@ -23,6 +23,36 @@ struct Triangle
 //Main function
 int main(void)
 {
-	struct point z;
+	struct point triangle[3]
+	readPoint(&triangle[0]);
+	readPoint(&triangle[1]);
+	readPoint(&triangle[2]);
 	return 0;
 }
+
+//Read point function
+void readPoint(struct point * ptr)
+{
+    printf("Enter a new point: \n");
+    printf("X-coord: ");
+    scanf("%d", &ptr->x);
+    printf("Y-coord: ");
+    scanf("%d", &ptr->y);
+}
+
+//Print the triangle
+void printTriangle(struct point *ptr)
+{
+    int i=0;
+    for (i=0; i<3; i++)
+    {
+
+    }
+}
+
+//Print the triangle
+void printPoint(struct point pt)
+{
+    printf("%d %d", pt.x, pt.y);
+}
+
