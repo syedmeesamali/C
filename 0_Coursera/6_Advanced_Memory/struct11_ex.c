5d6ac8eb1e3efefe0be779b83c9bdc3c00d51067
syedmeesamali <syedmeesamali@yahoo.com>
2024-05-24 20:44:18 +0300

    Some bug is still bugging - return to lectures of structs and pointers

--- 0_Coursera/6_Advanced_Memory/struct11_ex.c
+++ 0_Coursera/6_Advanced_Memory/struct11_ex.c
@@ -2,9 +2,6 @@
 Dynamic memory allocation for the structs.
 */
 #include <stdio.h>
-#include<stdlib.h>
-
-
 
 struct threedpoint
 {
@@ -14,24 +11,43 @@ struct threedpoint
 };
 
 void readPoint(struct threedpoint *pt);
+void printPolygon(struct threedpoint *ptr);
 void printPoint(struct threedpoint pt);
 
 //Main function
 int main(void)
 {
-	struct threedpoint * polygon[3];
+	struct threedpoint polygon[3];
     readPoint(&polygon[0]);
+    printPoint(polygon[0]);
     readPoint(&polygon[1]);
+    printPoint(polygon[1]);
     readPoint(&polygon[2]);
+    printPoint(polygon[2]);
+    printPolygon(polygon);
 	return 0;
 }
 
+//Read the coordinate points
 void readPoint(struct threedpoint *pt)
 {
-    printf("Enter x: ");
-    scanf("%lf", &pt->x);
-    printf("Enter y: ");
-    scanf("%lf", &pt->y);
-    printf("Enter z: ");
-    scanf("%lf", &pt->z);
+    printf("Enter x y z: ");
+    scanf("%lf %lf %lf", &pt->x, &pt->y, &pt->z);
+}
+
+//Print the whole structure
+void printPolygon(struct threedpoint *ptr)
+{
+    printf("\n-----------\n");
+    for (int i=0; i<3; i++)
+    {
+        printPoint(ptr[i]);
+    }
+}
+
+//Print the individual point
+void printPoint(struct threedpoint pt)
+{
+    printf("\n-----------\n");
+    printf("x = %lf, y = %lf, z = %lf \n", pt.x, pt.y, pt.z);
 }
