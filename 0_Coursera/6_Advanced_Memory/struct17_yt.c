acbb8d3ebc00796fe0707065e8c0839579b3fa26
syed <syedmeesamali@yahoo.com>
2024-06-23 23:41:57 +0300

    Linked list sereviews - Jammy (home)

--- 0_Coursera/6_Advanced_Memory/struct17_yt.c
+++ 0_Coursera/6_Advanced_Memory/struct17_yt.c
@@ -1,17 +1,20 @@
 #include<stdio.h>
 #include<stdlib.h>
 
-//Self referential struct
+//Self referential struct - Combining data with a link (self ref)
 struct node {
     int data;
     struct node *link;
 };
 
 
-
 //Main here
 int main()
 {
     struct node *head = NULL;
+    head = (struct node *)malloc(sizeof(struct node));      //Allocate memory for head
+    head -> data = 45;
+    head -> link = NULL;
+    printf("%d", head->data);
     return 0;
 }
