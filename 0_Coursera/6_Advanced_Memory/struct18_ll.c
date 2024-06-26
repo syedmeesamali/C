cdaba84e5ec26e6f0788d8b0022112772a2fa41c
syed <syedmeesamali@yahoo.com>
2024-06-26 23:55:43 +0300

    Excellent new reviews and some full program practice - Linked lists - Jammy(home)

--- 0_Coursera/6_Advanced_Memory/struct18_ll.c
+++ 0_Coursera/6_Advanced_Memory/struct18_ll.c
@@ -8,14 +8,28 @@ struct node {
 };
 
 void printData(struct node *ref);
+struct node *createNode(int x); 
+struct node *appendData(struct node *end, struct node *newpt);
 //Main here
 int main()
 {
     struct node *head = NULL;
+    struct node *start, *end, *newpt;       //Separate pointers for start and end
+    start = end;
     head = (struct node *)malloc(sizeof(struct node));      //Allocate memory for head
     head -> data = 45;
     head -> link = NULL;
     printData(head);
+    int nums, i, dat;
+    printf("How many points?");
+    scanf("%d", &nums);
+    for (i=0; i<nums; i++)
+    {
+        printf("\nData = ");
+        scanf("%d", &dat);
+        newpt = createNode(dat);
+    } 
+    end = appendData(end, newpt);
     return 0;
 }
 
@@ -29,4 +43,18 @@ void printData(struct node *start)
         printf("\n %d \n", ptr -> data);
         ptr = ptr -> link;
     }
+}
+
+//Create node
+struct node *createNode(int x)
+{
+    struct node *ptr;
+    ptr = (struct node *)malloc(sizeof(struct node));
+    ptr -> data = x;
+}
+
+//Append node
+struct node *appendData(struct node *start, struct node *end)
+{
+    //Implement append here
 }
