4daf5cafb111912cebdcb32cce5b40e560ef9ec5
syedmeesamali <syedmeesamali@yahoo.com>
2024-08-03 22:33:17 +0300

    Search function for STRUCT with nums

--- 0_Coursera/6_Advanced_Memory/struct27_digit_search.c
+++ 0_Coursera/6_Advanced_Memory/struct27_digit_search.c
@@ -6,27 +6,38 @@ struct digit {
     struct digit *next;
 };
 
-// Write your prototypes here
+#define false 0
+#define true 1
+typedef int bool;
+
+//Function prototypes
 struct digit *readNumber(void);
 struct digit *createDigit(int val);
 struct digit *append(struct digit *end, struct digit *newPos);
-int divisibleByThree(struct digit *start);
+struct digit *searchNumber(struct digit *start, int number);
 void printNumber(struct digit *ref);
 void freeNumber(struct digit *start);
 
+//Main is below
 int main(void) {
-    struct digit *start;
+    struct digit *start, *ptr;
+    int searchNum = 5;
+    printf("Please enter a number: ");
     start = readNumber();
-    printf("The number ");
     printNumber(start);
-    if (divisibleByThree(start))
-        printf("is divisible by 3.\n");
-    else
-        printf("is not divisible by 3.\n");
+    ptr = searchNumber(start, searchNum);
+    if (ptr != NULL)
+    {
+        printf("Found %d at %p.", searchNum, ptr);
+    } else
+    {
+        printf("%d not found !", searchNum);
+    }
     freeNumber(start);
     return 0;
 }
 
+//Create a new digit based on user input
 struct digit *createDigit(int dig) {
     struct digit *ptr;
     ptr = (struct digit *) malloc(sizeof(struct digit));
@@ -35,11 +46,13 @@ struct digit *createDigit(int dig) {
     return ptr;
 }
 
+//Append function
 struct digit * append(struct digit * end, struct digit * newDigptr) {
     end->next = newDigptr;
     return(end->next);
 }
 
+//Print a particular number
 void printNumber(struct digit *start) {
     struct digit * ptr = start;
     while (ptr!=NULL) {
@@ -49,23 +62,27 @@ void printNumber(struct digit *start) {
     printf("\n");
 }
 
+//Free the memory
 void freeNumber(struct digit *start) {
     struct digit * ptr = start;
     struct digit * tmp;
-    while (ptr!=NULL) {
+    while (ptr!=NULL)
+    {
         tmp = ptr->next;
         free(ptr);
         ptr = tmp;
     }
 }
 
+//Read a particular number
 struct digit *readNumber(void) {
     char c;
     int d;
     struct digit *start, *end, *newptr;
     start = NULL;
     scanf("%c", &c);
-    while (c != '\n') {
+    while (c != '\n')
+    {
         d = c-48;
         newptr = createDigit(d);
         if (start == NULL) {
@@ -79,22 +96,13 @@ struct digit *readNumber(void) {
     return(start);
 }
 
-// Write your divisibleByThree() function here
-int divisibleByThree(struct digit *start)
+//Search a number within entered numbers
+struct digit *searchNumber(struct digit *start, int number)
 {
-    int a = 0;
-    int sum = 0;
-    while (start != NULL)
+    struct digit *ptr = start;
+    while ((ptr != NULL) && (ptr -> num != number) )
     {
-        a = start -> num;
-        sum = sum + a;
-        start = start -> next;
+        ptr = ptr -> next;
     }
-    if (sum % 3 == 0)
-    {
-        return 1;
-    } else
-    {
-        return 0;
-    }
+    return ptr;
 };
