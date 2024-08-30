b3c77e5b61843b1f831a846a657781dfc4b57397
syedmeesamali <syedmeesamali@yahoo.com>
2024-08-30 23:34:55 +0300

    Study of DEBUG in C as well as GDB

--- 0_Coursera/7_Projects/3_You_Know_C/debug.c
+++ 0_Coursera/7_Projects/3_You_Know_C/debug.c
@@ -13,10 +13,10 @@ int main(void)
     int i, j, k, nread;
     nread = scanf("%d %d %d", &i, &j, &k);
 
-    #ifdef DEBUG
+
         fprintf(stderr, "Number of integers read = %i \n", nread);
         fprintf(stderr, "i = %i, j = %i, k = %i \n", i, j, k);
-    #endif // DEBUG
+    // DEBUG
     printf("%i \n", process(i, j, k));
     return 0;
 }
