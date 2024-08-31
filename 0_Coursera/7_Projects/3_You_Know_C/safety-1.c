b980b6d07a4ac370fb36834ef14543889e687e82
syedmeesamali <syedmeesamali@yahoo.com>
2024-08-31 23:51:42 +0300

    C safety issues review

---
+++ 0_Coursera/7_Projects/3_You_Know_C/safety-1.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+//Main function
+int main(void)
+{
+    int a, b;
+    b = -8;
+    if ((a=b) != 0)
+    {
+        puts("Value assigned!");
+    } else
+    {
+        puts("Nothing! Value is zero !");
+    }
+    return 0;
+}
