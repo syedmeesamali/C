d5a352d5637c5055f82b9e33cadf32380b4196ec
syed <syedmeesamali@yahoo.com>
2024-09-02 23:41:56 +0300

    Unsafe programming review in C - Jammy(home)

---
+++ 0_Coursera/7_Projects/3_You_Know_C/unsafe-array.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+    int arr[3] = {5, 7, 9};
+    int x;
+    for (x=0; x<10; x++)
+    {
+        printf("Array value is: %d \n", arr[x]);
+    }
+    return 0;
+}
