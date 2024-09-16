96e2ec6f36abeb7fa1982be4c7dc8fd5556cc07b
syedmeesamali <syedmeesamali@yahoo.com>
2024-09-16 23:45:06 +0300

    Some further play wiht graphics C

---
+++ 0_Coursera/7_Projects/3_You_Know_C/structs_vect.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<math.h>
+#include<string.h>
+#include<stdlib.h>
+
+
+//Structs
+typedef struct
+{
+    float x;
+    float y;
+} vec_2d;
+
+
+//Main program
+int main()
+{
+    printf("%d", sizeof(vec_2d));
+    return 0;
+}
