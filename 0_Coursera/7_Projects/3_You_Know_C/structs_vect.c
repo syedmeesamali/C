1d888804d0248aa3c83513ee41356fbd7fc2ffd7
syedmeesamali <syedmeesamali@yahoo.com>
2024-09-17 23:29:32 +0300

    Some grids game - C

--- 0_Coursera/7_Projects/3_You_Know_C/structs_vect.c
+++ 0_Coursera/7_Projects/3_You_Know_C/structs_vect.c
@@ -3,6 +3,9 @@
 #include<string.h>
 #include<stdlib.h>
 
+#define GRID_SIZE 20
+#define GRID_CENTER (GRID_SIZE / 2)
+
 
 //Structs
 typedef struct
@@ -11,10 +14,21 @@ typedef struct
     float y;
 } vec_2d;
 
+void clear_grid(char grid[GRID_SIZE][GRID_SIZE]);
 
 //Main program
 int main()
 {
     printf("%d", sizeof(vec_2d));
     return 0;
+}
+
+
+//Clear the grid function
+void clear_grid(char grid[GRID_SIZE][GRID_SIZE])
+{
+    for (int row = 0; row < GRID_SIZE; row++)
+    {
+        memset(grid[row], ' ', GRID_SIZE);
+    }
 }
