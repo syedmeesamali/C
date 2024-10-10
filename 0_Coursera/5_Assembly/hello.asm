a83a675c2fe7149f0f81c8ad442b82fa1b2ebcae
syed <syedmeesamali@yahoo.com>
2024-10-10 23:39:06 +0300

    Simple most assembly review again - Jammy(home)

--- 0_Coursera/5_Assembly/hello.asm
+++ 0_Coursera/5_Assembly/hello.asm
@@ -1,17 +1,13 @@
-global _start
-
-section .data
-message: db 'hello, world', 10
-
 section .text
+global _start
 _start:
-    mov rax, 1
-    mov rdi, 1
-    mov rsi, message
-    mov rdx, 14
-    syscall
-    
-    ;Exit syscall below
-    mov rax, 60
-    xor rdi, rdi
-    syscall
+    mov edx, len
+    mov ecx, msg
+    mov ebx, 1
+    mov eax, 4
+    int 0x80
+    mov eax, 1
+    int 0x80
+section .data
+msg db 'hello ali', 0xa
+len equ $ - msg
