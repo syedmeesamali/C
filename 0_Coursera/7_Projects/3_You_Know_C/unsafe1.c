cd149b1ca05c01d943503ddef901668750949772
syedmeesamali <syedmeesamali@yahoo.com>
2024-10-13 23:44:15 +0300

    Some great updates to challenge file for unsafe stuff and update main issues

--- 0_Coursera/7_Projects/3_You_Know_C/unsafe1.c
+++ 0_Coursera/7_Projects/3_You_Know_C/unsafe1.c
@@ -12,6 +12,12 @@ int main()
     FILE *fp;
 
     buffer = (char *)malloc(sizeof(char)*32);       //Buffer for 32 chars only
+    //1. Check for the buffer issues and allocation is done or NOT
+    if (buffer == NULL)
+    {
+        fprintf(stderr, "Unable to allocate space of 32 chars.\n");
+        exit(1);
+    }
     printf("Type some text: ");
     index = 0;
     while (ch = getchar())
@@ -25,9 +31,16 @@ int main()
     } //End of while
 
     fp = fopen(filename, "w");
+    //2. Check for file opening issues
+    if (fp == NULL)
+    {
+        fprintf(stderr, "Unable to open file %s for writing .... \n", filename);
+        return 1;
+    }
     fwrite(buffer, 1, index, fp);
+    free(buffer);
     fclose(fp);
     printf("\n%s is created!", filename);
     return 0;
-    
+
 }
