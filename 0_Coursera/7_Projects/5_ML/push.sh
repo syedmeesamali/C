335c8ad27368e1a08c3b2ed5be6b31e167111026
syed <syedmeesamali@yahoo.com>
2024-10-20 22:16:47 +0300

    Updates from C repo - Bash automated script

---
+++ 0_Coursera/7_Projects/5_ML/push.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+git add .
+git commit -m "Updates from C repo - Bash automated script"
+git push -u origin
+echo "All recent changes pushed to the online github repo. "
+echo "----------------"
