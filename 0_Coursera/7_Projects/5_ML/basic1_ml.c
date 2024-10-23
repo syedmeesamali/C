c633c2a0a7b6c7222557cfe665cf73ae3fe90f67
syedmeesamali <syedmeesamali@yahoo.com>
2024-10-23 23:34:46 +0300

    Small bugs in base code - still basics of ML in C

--- 0_Coursera/7_Projects/5_ML/basic1_ml.c
+++ 0_Coursera/7_Projects/5_ML/basic1_ml.c
@@ -2,7 +2,7 @@
 #include<stdlib.h>
 #include<time.h>
 
-#define train_count (sizeof(train))/(sizeof(train[0]));
+#define train_count (sizeof(train))/(sizeof(train[0]))
 
 float train[][2] = {
     {0, 0},             //For zero predict 0
@@ -23,14 +23,13 @@ int main()
 {
     // y = x * w
     //srand(time(0));
-    srand(69);          //Some fixed rand with a seed
-    float w = random_float()*10.0f;      //multiply by 10 to get random from 0 - 10
-    for (size_t i=0; i<train_count; i++)
+    srand(69);
+    float w = random_float()*100.0f;
+    for (size_t i=0; i<train_count; ++i)
     {
         float x = train[i][0];
         float y = x * w;            //As per original logic of ML
         printf("actual: %f, expected: %f\n", y, train[i][0]);
     }
-    printf("ML in C. Rand number = %f \n", w);
     return 0;
 }
