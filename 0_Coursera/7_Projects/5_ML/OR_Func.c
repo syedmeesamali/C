c2d62850cc9e6b4dd74551691de85c6c09f84bf5
syedmeesamali <syedmeesamali@yahoo.com>
2024-11-06 23:45:55 +0300

    BIAS is introduced in our OR ML model in C

--- 0_Coursera/7_Projects/5_ML/OR_Func.c
+++ 0_Coursera/7_Projects/5_ML/OR_Func.c
@@ -25,14 +25,14 @@ float random_float(void)
 }
 
 //Main cost function - w and b are main parameters (b as bias)
-float cost(float w1, float w2)
+float cost(float w1, float w2, float b)
 {
     float result = 0.0f;
     for (size_t i=0; i<train_count; ++i)
     {
         float x1 = train[i][0];
         float x2 = train[i][1];
-        float y = sigmoid(x1 * w1 + x2 * w2);
+        float y = sigmoid(x1 * w1 + x2 * w2) + b;
         float d = y - train[i][2];
         result += d * d;
     }
@@ -50,17 +50,23 @@ int main()
 
     float w1 = random_float();
     float w2 = random_float();
+    float b = random_float();           //ADD the BIAS term also
+
     float eps = 0.1;
     float rate = 0.1;
     for (size_t i=0; i<1000; ++i)
     {
-        float c = cost(w1, w2);
-        printf("w1 = %f, w2 = %f, c = %f\n", w1, w2, c);
-        float dw1 = (cost(w1 + eps, w2) - c)/ eps;
-        float dw2 = (cost(w1, w2 + eps) - c) / eps;
+        float c = cost(w1, w2, b);
+        printf("w1 = %f, w2 = %f, b = %f, c = %f\n", w1, w2, b, c);
+        float dw1 = (cost(w1 + eps, w2, b) - c)/ eps;
+        float dw2 = (cost(w1, w2 + eps, b) - c) / eps;
+        float db = (cost(w1, w2, b + eps) - c) / eps;
         w1 -= rate * dw1;
         w2 -= rate * dw2;
+        b -= rate * db;
     }
+
+    printf("w1 = %f, w2 = %f, b = %f, c = %f\n", w1, w2, b, cost(w1, w2, b));
 
     for (size_t i=0; i<2; ++i)
     {
