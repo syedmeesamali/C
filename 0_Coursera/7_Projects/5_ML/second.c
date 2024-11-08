ac00553f4ce8a13bc7b1e9e59a08ab067093aeb9
syed <syedmeesamali@yahoo.com>
2024-11-08 23:56:49 +0300

    ML in C continuation - Jammy(home)

--- 0_Coursera/7_Projects/5_ML/second.c
+++ 0_Coursera/7_Projects/5_ML/second.c
@@ -1,6 +1,17 @@
 #include<stdio.h>
 #include<stdlib.h>
 
+//Our simple training data
+float train[][2] = 
+{
+    {0, 0},
+    {1, 2},
+    {2, 4},
+    {3, 6},
+    {4, 8}
+};
+
+//Get random floats
 float rand_float(void)
 {
     return (float) rand() / (float) RAND_MAX;
@@ -8,8 +19,19 @@ float rand_float(void)
 
 int main()
 {
-    // y = x * w + b
+    srand(70);
+    float result=0.0f;
     float w = rand_float();
-    printf("Some random float is %f.\n", w);
+    // y = x * w + b
+    
+    for (size_t i=0; i<10; ++i)
+    {
+        float x = train[i][0];
+        float y = x * w;
+        float d = y - train[i][0];
+        result += d * d;
+        printf("x val = %f, y = %f, d = %f, result = %f.\n", x, y, d, result);
+    }
+
     return 0;
 }
