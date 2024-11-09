c5bcb9dd4a5328c3242763b850944a095e23c188
syed <syedmeesamali@yahoo.com>
2024-11-09 23:55:33 +0300

    Still playing with ML course in C on VM - Jammy(home)

--- 0_Coursera/7_Projects/5_ML/second.c
+++ 0_Coursera/7_Projects/5_ML/second.c
@@ -1,6 +1,5 @@
 #include<stdio.h>
 #include<stdlib.h>
-
 //Our simple training data
 float train[][2] = 
 {
@@ -22,16 +21,15 @@ int main()
     srand(70);
     float result=0.0f;
     float w = rand_float();
-    // y = x * w + b
-    
-    for (size_t i=0; i<10; ++i)
+    // y = x * w + b    
+    printf("\n Traing data: train[3][1] = %f \n", train[3][1]);
+    for (size_t i=0; i<5; ++i)
     {
         float x = train[i][0];
         float y = x * w;
         float d = y - train[i][0];
         result += d * d;
-        printf("x val = %f, y = %f, d = %f, result = %f.\n", x, y, d, result);
+        printf("x = %f, y = %f, dist = %f, train[0][i] = %f, result = %f \n", x, y, d, train[0][i], result);
     }
-
     return 0;
 }
