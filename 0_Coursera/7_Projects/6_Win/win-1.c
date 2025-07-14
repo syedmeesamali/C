1c7e2a29d0c8a9739e22f44af212ccd6f435dabb
syedmeesamali <syedmeesamali@yahoo.com>
2025-07-14 23:53:01 +0300

    Some more play with win32api and Win Classes

--- 0_Coursera/7_Projects/6_Win/win-1.c
+++ 0_Coursera/7_Projects/6_Win/win-1.c
@@ -1,5 +1,27 @@
 #include<Windows.h>
 
+const char g_szClassName[] = "myWindowClass";
+
+//The window procedure
+LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lparam)
+{
+    switch(msg)
+    {
+    case WM_CLOSE:
+        DestroyWindow(hwnd);
+        break;
+    case WM_DESTROY:
+        PostQuitMessage(0);
+        break;
+    default:
+        return DefWindowProc(hwnd, msg, wParam, lparam);
+    }
+    return 0;
+}
+
+
+
+//Main function
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
 {
     MessageBox(NULL, "Good bye cruel world.", "Note", MB_OK);
